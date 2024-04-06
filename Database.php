52a1e703618dcdd0e7e7d791c2ab64c659f21d2a
Матвей Кучеров <matrix-elf@yandex.ru>
2024-04-06 17:21:02 +0700

    test 3 works

--- Database.php
+++ Database.php
@@ -24,14 +24,52 @@ class Database implements DatabaseInterface
         $spotIndex = mb_strpos($buildedQuery, "?");
         $nextArg = $args[$argIndex];
 
+        $modifiers = ["d", "f", "a", "#"];
+
         while($spotIndex !== false && $argIndex++ < $argsCount) {
             $beforeSpot = substr($buildedQuery, 0, $spotIndex);
-            $afterSpot = substr($buildedQuery, $spotIndex + 1);
+            $modifier = $buildedQuery[$spotIndex + 1];
+            $haveModifier = in_array($modifier, $modifiers, true);
+            $afterSpot = substr($buildedQuery, $spotIndex + ($haveModifier? 2 : 1));
 
             $buildedQuery = $beforeSpot;
-            if (is_string($nextArg)) {
+
+            if (!$haveModifier) {
                 $buildedQuery .= "'" . $this->mysqli->real_escape_string($nextArg) . "'";
             }
+
+            if ($haveModifier) {
+                switch ($modifier) {
+                    case '#':
+                        if (is_array($nextArg)) {
+                            $buildedQuery .= implode(
+                                ", ",
+                                array_map(
+                                    fn ($a) => "`". $this->mysqli->real_escape_string((string) $a)."`",
+                                    $nextArg
+                                )
+                            );
+                        } else {
+                            $buildedQuery .= "`". $this->mysqli->real_escape_string((string) $nextArg)."`";
+                        }
+                        break;
+
+                    case 'd':
+                        $buildedQuery .= (int) $nextArg;
+                        break;
+
+                    case 'f':
+                        break;
+
+                    case 'a':
+                        break;
+
+                    default: // never here
+                        $buildedQuery .= "'" . $this->mysqli->real_escape_string((string) $nextArg) . "'";
+                        break;
+                }
+            }
+
             $buildedQuery .= $afterSpot;
 
 
